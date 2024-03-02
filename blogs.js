6b0b9099668cdea575db9f43bb82f39e7c57b781
Kyle <1kylejohns@gmail.com>
2024-03-01 22:30:47 -0800

    Added Blogs

---
+++ blogs.js
@@ -0,0 +1,40 @@
+const dialog1 = document.getElementById("jan302024");
+const show1Button = document.getElementById("show1Dialog");
+const hide1Button = document.getElementById("hide1Dialog");
+
+show1Button.addEventListener("click", () => {
+    jan302024.showModal();
+  });
+
+
+hide1Button.addEventListener("click", () => {
+    jan302024.close ();
+  });
+
+
+  const dialog2 = document.getElementById("jan112024");
+const show2Button = document.getElementById("show2Dialog");
+const hide2Button = document.getElementById("hide2Dialog");
+
+show2Button.addEventListener("click", () => {
+    jan112024.showModal();
+  });
+
+
+hide2Button.addEventListener("click", () => {
+    jan112024.close ();
+  });
+
+
+  const dialog3 = document.getElementById("sept202023");
+const show3Button = document.getElementById("show3Dialog");
+const hide3Button = document.getElementById("hide3Dialog");
+
+show3Button.addEventListener("click", () => {
+    sept202023.showModal();
+  });
+
+
+hide3Button.addEventListener("click", () => {
+    sept202023.close ();
+  });
